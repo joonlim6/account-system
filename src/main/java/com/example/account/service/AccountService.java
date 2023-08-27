9dd7487d02c6383399d315406b66d7a464f10f73
joonlim6 <limjoon629@gmail.com>
2023-08-27 22:29:59 +0900

    계좌 생성 api

--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -1,25 +1,55 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.domain.AccountUser;
+import com.example.account.dto.AccountDto;
+import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+import static com.example.account.type.AccountStatus.IN_USE;
+import static com.example.account.type.ErrorCode.MAX_ACCOUNT_PER_USER_10;
+import static com.example.account.type.ErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final AccountUserRepository accountUserRepository;
 
     @Transactional
-    public void createAccount() {
-        Account account = Account.builder()
-                .accountNumber("40000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
-        accountRepository.save(account);
+    public AccountDto createAccount(Long userId, Long initialBalance) {
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(USER_NOT_FOUND));
+
+        validateCreateAccount(accountUser);
+
+        String accountNumber = accountRepository.findFirstByOrderByIdDesc()
+                .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
+                .orElse("1000000000");
+
+        return AccountDto.fromEntity(
+                accountRepository.save(
+                        Account.builder()
+                                .accountUser(accountUser)
+                                .accountStatus(IN_USE)
+                                .accountNumber(accountNumber)
+                                .balance(initialBalance)
+                                .registeredAt(LocalDateTime.now())
+                                .build()
+                )
+        );
+    }
+
+    private void validateCreateAccount(AccountUser accountUser) {
+        if (accountRepository.countByAccountUser(accountUser) >= 10) {
+            throw new AccountException(MAX_ACCOUNT_PER_USER_10);
+        }
     }
 
     @Transactional
