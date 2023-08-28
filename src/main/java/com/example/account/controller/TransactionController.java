8b7d1a22a0267d82ef8ec3a3b9e6e4347bfc288f
joonlim6 <limjoon629@gmail.com>
2023-08-28 19:11:46 +0900

    중복 거래 방지

--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.account.controller;
 
+import com.example.account.aop.AccountLock;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.QueryTransactionResponse;
 import com.example.account.dto.UseBalance;
@@ -18,8 +19,10 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/transaction/use")
-    public UseBalance.Response useBalance(@Valid @RequestBody UseBalance.Request request) {
+    @AccountLock
+    public UseBalance.Response useBalance(@Valid @RequestBody UseBalance.Request request) throws InterruptedException {
         try{
+            Thread.sleep(3000L);
             return UseBalance.Response.from(
                     transactionService.useBalance(request.getUserId(),
                             request.getAccountNumber(), request.getAmount())
@@ -37,6 +40,7 @@ public class TransactionController {
     }
 
     @PostMapping("/transaction/cancel")
+    @AccountLock
     public CancelBalance.Response cancelBalance(@Valid @RequestBody CancelBalance.Request request) {
         try {
             return CancelBalance.Response.from(
